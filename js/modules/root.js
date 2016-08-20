165272ae674fcc1c38c40d0bcb90ab8e774ba6f2
Jesús Ricaño <racing_rica@hotmail.com>
2016-08-19 20:45:00 -0500

    v1 ejemplo 1 y 2

--- js/modules/root.js
+++ js/modules/root.js
@@ -1,4 +1,19 @@
+/*****************************************************************/
+//Utilizare eñ mismo modulo y controlador para todos los ejemplos//
+/*****************************************************************/
+
 angular.module('root', [])
     .controller("index", ["$scope", function ($scope) {
-        $scope.message = "Hola mundo desde js/root.js Angular";
+        $scope.message = "Hola a todos desde angular";
+        $scope.messageNombre = "Jesús";
+
+
+        // ejemplo 2 
+        $scope.yourname;
+        $scope.favoriteWord;
+        $scope.favoriteColor;
+        $scope.favoriteShape;
     }]);
+
+
+
