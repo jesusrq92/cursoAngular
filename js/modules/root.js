76de7a0cfca984372b3814d068ff75c20e1887c6
Jesús Ricaño <racing_rica@hotmail.com>
2016-08-26 23:33:12 -0500

    modificaciones en la tabla ngRepeat

--- js/modules/root.js
+++ js/modules/root.js
@@ -24,11 +24,14 @@ angular.module('root', ["filters"])
 
 		//ejemplo 4
 		$scope.products = [
-			{id: 1, name: "Hockey puck", price: 14},
-			{id: 2, name: "Golf club", price: 19},
-			{id: 3, name: "Baseball bat", price: 8},
-			{id: 4, name: "Lacrosse stick", price: 10}
+			{id: 1, name: "Hockey puck", price: 14, pubdate: new Date('2014', '03', '08'), cover: "images/picture.png", likes:0},
+			{id: 2, name: "Golf club", price: 19, pubdate: new Date('2014', '03', '08'), cover: "images/picture.png", likes:0},
+			{id: 3, name: "Baseball bat", price: 8, pubdate: new Date('2014', '03', '08'), cover: "images/picture.png", likes:0},
+			{id: 4, name: "Lacrosse stick", price: 10, pubdate: new Date('2014', '03', '08'), cover: "images/picture.png", likes:0}
 		];
+        $scope.plusOne = function(index) {
+            $scope.products[index].likes += 1; //accedemos a likes
+        };
 
 		$scope.records = [
 	        "Alfreds Futterkiste",
